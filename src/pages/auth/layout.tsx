c54b929c6a49d8eca4769355b81c7c342cd76e7f
Ryota Ikezawa <pavegy@gmail.com>
2023-09-30 17:30:46 +0900

    :recycle: Update

---
+++ src/pages/auth/layout.tsx
@@ -0,0 +1,20 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+
+export function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
+      <AspectRatio ratio={16 / 9}>
+        <Image
+          src="https://images.unsplash.com/photo-1660099789632-ed5fae6189af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2969&q=80"
+          alt="Photo by Ness fu"
+          fill
+          className="rounded-md object-cover"
+        />
+      </AspectRatio>
+      <main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AuthLayout } from "./layout";
+
+export default function SignIn() {
+  const { data: sessionData } = useSession();
+
+  return (
+    <AuthLayout>
+      <Card className="mx-auto text-center w-[350px]">
+        <Button
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          {sessionData ? "Logout" : "Login"}
+        </Button>
+      </Card>
+    </AuthLayout>
+  );
+}

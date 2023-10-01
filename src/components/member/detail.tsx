a79fa3fc85af2e09cea55189c3e32ff8fa6a4b5c
Ryota Ikezawa <pavegy@gmail.com>
2023-10-01 10:13:29 +0900

    :recycle: Update

---
+++ src/components/member/detail.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { api } from "~/utils/api";
+
+type Props = {
+  id: string;
+};
+export const MemberDetail: FC<Props> = (props) => {
+  const { id } = props;
+  const { data: member, isLoading } = api.member.get.useQuery({ id: id });
+
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <h1 className="text-2xl text-center">{member?.name}</h1>
+          <LineChart
+            className="mx-auto ma-4"
+            width={800}
+            height={300}
+            data={member?.results.map((result) => {
+              return {
+                name: `${result.game.name}${result.sequence + 1}局`,
+                rank: result.rank,
+              };
+            })}
+          >
+            <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis
+              type="number"
+              allowDecimals={false}
+              reversed
+              padding={{ top: 5, bottom: 5 }}
+              interval={1}
+              domain={[1, 4]}
+            />
+            <Tooltip />
+            <Line type="monotone" dataKey="rank" stroke="#8884d8" />
+          </LineChart>
+        </>
+      )}
+    </>
+  );
+};
--- src/pages/members/[id].tsx
+++ src/pages/members/[id].tsx
@@ -1,15 +1,11 @@
-import { Car } from "lucide-react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Line, LineChart, Tooltip, XAxis, CartesianGrid } from "recharts";
-import { api } from "~/utils/api";
+import { MemberDetail } from "~/components/member/detail";
 
-type Props = { id: string }
-export default function Member() {
+export default function () {
   const router = useRouter();
   const { id } = router.query;
-  const { data: member, isLoading } = api.member.get.useQuery({ id: id });
 
   return (
     <>
@@ -23,22 +19,7 @@ export default function Member() {
       </Head>
       <main>
         <div className="mx-auto m-4 container">
-          <h1 className="text-2xl text-center">{member?.name}</h1>
-          {!isLoading && (
-            <LineChart
-              className="mx-auto w-[800px]"
-              width={800}
-              height={300}
-              data={member?.results.map((result) => {
-                return { name: `${result.game.name}${result.sequence + 1}局`, rank: result.rank, point: result.point };
-              })}
-            >
-              <Line type="monotone" dataKey="point" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-              <XAxis dataKey="name" />
-              <Tooltip />
-            </LineChart>
-          )}
+          {id && <MemberDetail id={id} />}
         </div>
         <Link href="/">戻る</Link>
       </main>
